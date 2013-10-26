8d1700b792a35421f531a661b5a144bd0c61b510
Ørjan Blom <blom@blom.tv>
2013-10-26 14:21:34 +0200

    Dry up the spec

--- spec/defines/extract_spec.rb
+++ spec/defines/extract_spec.rb
@@ -1,41 +1,30 @@
 require "spec_helper"
 
 describe "extract" do
+  DEFAULT_PARAMS = {
+    :file    => "/opt/example-1.2.3.tar.gz",
+    :url     => "http://example.com/example-1.2.3.tar.gz",
+    :target  => "/opt",
+    :creates => "/opt/example-1.2.3",
+  }
+
+  DEFAULT_PARAMS.each { |key, val| let(key) { val } }
   let(:title) { "some description" }
-
-  let(:file)    { "/opt/example-1.2.3.tar.gz" }
-  let(:url)     { "http://example.com/example-1.2.3.tar.gz" }
-  let(:target)  { "/opt" }
-  let(:creates) { "/opt/example-1.2.3" }
-
-  let(:params) {{
-    :file    => file,
-    :url     => url,
-    :target  => target,
-    :creates => creates,
-  }}
-
+  let(:params) { DEFAULT_PARAMS }
   let(:archive_file) { file }
 
-  describe "file" do
-    context "present" do
-      it { should contain_exec("download #{url} to #{file}") }
-    end
+  def params_with(key, val)
+    DEFAULT_PARAMS.merge key => val
+  end
 
-    context "not present" do
-      let(:params) {{
-        :url     => url,
-        :target  => target,
-        :creates => creates,
-      }}
-
-      it { should contain_exec("download #{url} to #{title}") }
-    end
+  def params_without(*keys)
+    DEFAULT_PARAMS.reject { |key, val| keys.include? key }
   end
 
   it { should contain_exec("download #{url} to #{archive_file}").
               with_command("curl -L #{url} -o #{archive_file}").
-              with_onlyif(["test ! -e #{archive_file}", "test ! -e #{creates}"]) }
+              with_onlyif(["test ! -e #{archive_file}",
+                           "test ! -e #{creates}"]) }
 
   it { should contain_exec("extract #{archive_file}").
               with_command("tar xf #{archive_file} -C #{target}").
@@ -44,46 +33,38 @@ describe "extract" do
 
   it { should_not contain_file(archive_file) }
 
-  describe "when taropts is 'o'" do
-    let(:params) {{
-      :file    => file,
-      :target  => target,
-      :creates => creates,
-      :taropts => 'o',
-    }}
+  describe "file" do
+    context "when present" do
+      it { should contain_exec("download #{url} to #{file}") }
+    end
 
+    context "when not present" do
+      let(:params) { params_without(:file) }
+      it { should contain_exec("download #{url} to #{title}") }
+    end
+  end
+
+  describe "when taropts is 'o'" do
+    let(:params) { params_with(:taropts, 'o') }
     it { should contain_exec("extract #{archive_file}").
                 with_command("tar xof #{archive_file} -C #{target}") }
   end
 
   describe "when 'purge' is true" do
-    let(:params) {{
-      :file    => file,
-      :target  => target,
-      :creates => creates,
-      :purge   => true,
-    }}
-
+    let(:params) { params_with(:purge, true) }
     it { should contain_file(archive_file).
                 with_ensure("absent").
                 with_require(/\AExec\[extract #{archive_file}\]/) }
   end
 
   describe "when 'url' is missing" do
-    let(:params) {{
-      :target  => target,
-      :creates => creates,
-    }}
-
+    let(:params) { params_without(:url) }
     it { should_not contain_exec("download #{url} to #{title}") }
-    it { should contain_exec("extract #{title}").without_require }
+    it { should contain_exec("extract #{archive_file}").without_require }
   end
 
   describe "when 'target' is missing" do
-    let(:params) {{
-      :creates => creates,
-    }}
-
+    let(:params) { params_without(:target) }
     specify do
       expect {
         should contain_file(title)
@@ -92,10 +73,7 @@ describe "extract" do
   end
 
   describe "when 'creates' is missing" do
-    let(:params) {{
-      :target => target,
-    }}
-
+    let(:params) { params_without(:creates) }
     specify do
       expect {
         should contain_file(title)
